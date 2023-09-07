e8c9546f6633a2be5e865e144f6568f1375f3e05
Raa1F69 <142647446+Raa1F69@users.noreply.github.com>
2023-09-07 00:27:01 -0600

    Add files via upload

---
+++ LINQNumberFilterExample/LINQNumberFilterExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQNumberFilterExample 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            int[] scores = { 97, 92, 81, 60 };
+
+            var highScores = from score in scores
+                             where score > 80
+                             select score;
+          
+            foreach (var highScore in highScores)
+            {
+                Console.WriteLine(highScore);
+            }
+        }
+    }
+}
+
---
+++ LINQNumberFilterExample2/LINQNumberFilterExample2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+        var filteredAndSortedScores = from score in scores
+                                      where score > 80
+                                      orderby score descending
+                                      select score;
+
+        foreach (var score in filteredAndSortedScores)
+        {
+            Console.WriteLine(score);
+        }
+    }
+}
